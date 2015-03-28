5d6714be1100c9352aa02b68ebe5bbad862fde7d
Milos Bejda <mbejda@live.com>
2015-03-28 15:24:00 -0400

    Save

--- hello/platforms/ios/www/cordova_plugins.js
+++ hello/platforms/ios/www/cordova_plugins.js
@@ -22,13 +22,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/com.synconset.cordovaHTTP/www/cordovaHTTP.js",
-        "id": "com.synconset.cordovaHTTP.CordovaHttpPlugin",
-        "clobbers": [
-            "plugins.CordovaHttpPlugin"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
         "id": "org.apache.cordova.geolocation.Coordinates",
         "clobbers": [
@@ -54,6 +47,20 @@ module.exports = [
         "id": "org.apache.cordova.geolocation.geolocation",
         "clobbers": [
             "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/com.synconset.cordovaHTTP/www/cordovaHTTP.js",
+        "id": "com.synconset.cordovaHTTP.CordovaHttpPlugin",
+        "clobbers": [
+            "plugins.CordovaHttpPlugin"
+        ]
+    },
+    {
+        "file": "plugins/plugin.http.request/www/http-request.js",
+        "id": "plugin.http.request.phonegap-http-requst",
+        "clobbers": [
+            "cordova.plugins.http-request"
         ]
     },
     {
@@ -191,8 +198,9 @@ module.exports.metadata =
 {
     "com.radiusnetworks.cordova.proximitykit": "0.5.1",
     "org.apache.cordova.console": "0.2.13",
-    "com.synconset.cordovaHTTP": "0.1.4",
     "org.apache.cordova.geolocation": "0.3.12",
+    "com.synconset.cordovaHTTP": "0.1.4",
+    "plugin.http.request": "1.0.4",
     "org.apache.cordova.file": "0.2.5"
 }
 // BOTTOM OF METADATA
