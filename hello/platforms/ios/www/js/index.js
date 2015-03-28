b11fd38105082272e115263f04ecb4a6640d6a2f
Milos Bejda <mbejda@live.com>
2015-03-28 13:34:32 -0400

    save

--- hello/platforms/ios/www/js/index.js
+++ hello/platforms/ios/www/js/index.js
@@ -1,4 +1,4 @@
-/*
+       /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -102,3 +102,8 @@ var app = {
         });
     }
 };
+
+
+
+            app.initialize();
+
